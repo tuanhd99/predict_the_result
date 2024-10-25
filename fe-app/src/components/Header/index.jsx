8bafa4bf85afd6fa1f4737b156c2de12d9561851
Kane.Do <Kane.Do@avepoint.com>
2024-10-25 10:29:13 +0700

    [0001] Fix css

--- fe-app/src/components/Header/index.jsx
+++ fe-app/src/components/Header/index.jsx
@@ -1,15 +1,16 @@
-import { Button, Modal, Popconfirm } from 'antd';
+import { Button, Modal } from 'antd';
 import { useState } from 'react';
+import { FaBitcoin } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa6';
 import Login from '../../pages/Login/Login';
 import Register from '../../pages/Register/Register';
 import { useAuthData } from '../../stores';
-import { FaBitcoin } from 'react-icons/fa';
-import { FaBars } from 'react-icons/fa6';
 
 function Header() {
     const [isLogin, setIsLogin] = useState(false);
     const [isRegister, setIsRegister] = useState(false);
     const { authData, setAuthData } = useAuthData();
+    const [isLogout, setIsLogout] = useState(false);
 
     const handleOk = () => {};
 
@@ -31,63 +32,66 @@ function Header() {
 
     const handleLogout = () => {
         setAuthData({});
+        setIsLogout(false);
         localStorage.removeItem('auth');
     };
 
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 bg-blue-900 h-24 flex justify-between items-center px-4">
+        <header className="fixed top-0 left-0 right-0 z-50 bg-blue-900 px-4 h-24">
+            <div className="grid grid-cols-3 items-center h-24 bg-blue-900 px-8">
+                <FaBars size={30} color="#ffffff" className="cursor-pointer justify-self-start" />
+                <div className="uppercase text-5xl text-white text-center">Dự Đoán Tỉ Số</div>
+                <div className="justify-self-end">
+                    {authData.isAuthenticated ? (
+                        <div className="flex items-center gap-6">
+                            <div className="flex flex-col items-center gap-2 text-white">
+                                <span className="text-xl font-bold">{authData.data.email}</span>
+                                <span className="text-2xl flex items-center justify-center gap-3">
+                                    <strong>Coin: </strong>
+                                    <span className="text-amber-400 flex items-center gap-1 mt-1">
+                                        {authData.data.coin}
+                                        <FaBitcoin />
+                                    </span>
+                                </span>
+                            </div>
+                            <Button
+                                type="primary"
+                                className="text-xl bg-white text-blue-800 font-bold uppercase rounded-xl "
+                                onClick={() => setIsLogout(true)}
+                            >
+                                Log out
+                            </Button>
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-2 gap-5">
+                            <Button
+                                onClick={handleOpen}
+                                className="bg-white py-2 w-32 text-blue-800 rounded-xl font-bold uppercase text-xl"
+                                type="primary"
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                onClick={handleOpenRegister}
+                                className="bg-white py-2 w-32 text-blue-800 rounded-xl font-bold uppercase text-xl"
+                                type="primary"
+                            >
+                                Register
+                            </Button>
+                        </div>
+                    )}
+                </div>
+            </div>
+
             <Modal open={isLogin} onOk={handleOk} onCancel={handleCancel} footer={<></>}>
                 <Login handleCancel={handleCancel} />
             </Modal>
             <Modal open={isRegister} onOk={handleOk} onCancel={handleCancelRegister} footer={<></>}>
                 <Register handleCancel={handleCancelRegister} />
             </Modal>
-            <FaBars size={30} color="#ffffff" />
-            <div className="uppercase text-5xl text-white">Dự Đoán Tỉ Số</div>
-            <div className="flex gap-4">
-                {authData.isAuthenticated ? (
-                    <div className="flex items-center gap-6">
-                        <div className="flex flex-col items-center gap-2">
-                            <span className="text-xl font-bold text-gray-800">{authData.data.email}</span>
-                            <span className="text-2xl flex items-center jusitfy-center gap-3">
-                                <strong>coin: </strong>
-                                <span className="text-2xl text-amber-400 flex items-center jusitfy-center gap-1 mt-1">
-                                    {authData.data.coin}
-                                    <FaBitcoin />{' '}
-                                </span>
-                            </span>
-                        </div>
-                        <Popconfirm
-                            title="Đăng xuất"
-                            description="Bạn có chắc chắn muốn đăng xuất không?"
-                            onConfirm={handleLogout}
-                            okText="Đồng ý"
-                            cancelText="Hủy bỏ"
-                        >
-                            <Button type="primary" className="text-xl font-bold">
-                                Đăng xuất
-                            </Button>
-                        </Popconfirm>
-                    </div>
-                ) : (
-                    <>
-                        <Button
-                            onClick={handleOpen}
-                            className="bg-white py-2 w-32 text-blue-800 rounded-xl font-bold uppercase text-xl"
-                            type="primary"
-                        >
-                            Login
-                        </Button>
-                        <Button
-                            onClick={handleOpenRegister}
-                            className="bg-white py-2 w-32 text-blue-800 rounded-xl font-bold uppercase text-xl"
-                            type="primary"
-                        >
-                            Register
-                        </Button>
-                    </>
-                )}
-            </div>
+            <Modal title="Log out" open={isLogout} onOk={handleLogout} onCancel={() => setIsLogout(false)}>
+                <p>Are you sure you want to log out?</p>
+            </Modal>
         </header>
     );
 }
